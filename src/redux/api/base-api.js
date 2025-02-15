0eadc42d0f732cd5d61f1d58cd485024fb1718be
iammeskat <meskatcse@gmail.com>
2025-02-15 22:14:13 +0600

    fix: change base url

--- src/redux/api/base-api.js
+++ src/redux/api/base-api.js
@@ -1,7 +1,7 @@
 import { getCookie } from '@/utils/session';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseUrl = "http://localhost:3001/api";
+const baseUrl = NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001/api";
 
 const baseQuery = fetchBaseQuery({
 	baseUrl,
